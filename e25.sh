f8ae651963cfbcedbf862a7190ed418ec58a4251
JimmyG <jimmy.m.gong@gmail.com>
2016-10-23 17:25:21 +0800

    install emacs25.1 x

--- e25.sh
+++ e25.sh
@@ -1,3 +1,5 @@
+# sudo apt-get purge emacs emacs23
+sudo apt-get remove --auto-remove emacs23-lucid emacs23-nox
 wget http://ftpmirror.gnu.org/emacs/emacs-25.1.tar.xz
 tar xf emacs-25.1.tar.xz
 cd emacs-25.1
@@ -6,3 +8,6 @@ sudo apt-get install -y libwebkitgtk-3.0-dev
 ./configure --with-xwidgets --with-x-toolkit=gtk3 --with-modules
 make -j5
 sudo make install
+
+sudo apt-get install emacs texlive-latex-recommended texlive-latex-extra texlive-fonts-recommended
+sudo apt-get install chktex
