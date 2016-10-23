34832c579b2d4144d83adbdbe8826abb1374cfbc
JimmyG <jimmy.m.gong@gmail.com>
2016-10-23 17:03:35 +0800

    install emacs25.1 x

--- e25.sh
+++ e25.sh
@@ -1,8 +1,8 @@
 wget http://ftpmirror.gnu.org/emacs/emacs-25.1.tar.xz
 tar xf emacs-25.1.tar.xz
 cd emacs-25.1
-apt-get build-dep -y emacs24
-apt-get install -y libwebkitgtk-3.0-dev
+sudo apt-get build-dep -y emacs24
+sudo apt-get install -y libwebkitgtk-3.0-dev
 ./configure --with-xwidgets --with-x-toolkit=gtk3 --with-modules
 make -j5
-make install
+sudo make install
