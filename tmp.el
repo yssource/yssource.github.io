97b141f0b89c3b18da07c23813720bc5e749cdc4
JimmyG <jimmy.m.gong@gmail.com>
2016-10-23 22:41:24 +0800

    debug renderer.js

---
+++ tmp.el
@@ -0,0 +1,11 @@
+(progn
+  (setq org-export-allow-bind-keywords t)
+  (goto-line 0)
+  ;; (insert "${config.org.common}\n")
+  ;; reload org setting https://lists.gnu.org/archive/html/emacs-orgmode/2014-01/msg00648.html
+  (normal-mode)
+  (unless (derived-mode-p "org-mode")
+    (org-mode))
+  ;; (org-html-export-as-html nil nil nil nil nil nil)
+  (org-html-export-as-html nil nil nil nil nil)
+  (message "${flag}%s${flag}" (buffer-string)))
